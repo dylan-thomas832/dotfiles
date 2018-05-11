484771bf53385461492bd91828bc63e49201c77f
Dylan Thomas <dylan93@vt.edu>
2018-05-11 04:13:20 -0400

    Refactored colors_vim into the vim directory.
    Moved vim colors submodule to inside the vim/ directory.
    Updated vim_setup.sh script accordingly.

--- bin/vim_setup.sh
+++ bin/vim_setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+## TODO: Make this agnostic to the number of repos in colors_vim
+##          Should be like a for each statement
+
 # Copying color files into vim directory 
-cp ~/.dotfiles/colors_vim/badwolf/colors/badwolf.vim ~/.dotfiles/vim/colors/
-cp ~/.dotfiles/colors_vim/molokai/colors/molokai.vim ~/.dotfiles/vim/colors/
-cp ~/.dotfiles/colors_vim/vim-colors-solarized/colors/solarized.vim ~/.dotfiles/vim/colors/
+cp ~/.dotfiles/vim/colors_vim/badwolf/colors/badwolf.vim ~/.dotfiles/vim/colors/
+cp ~/.dotfiles/vim/colors_vim/molokai/colors/molokai.vim ~/.dotfiles/vim/colors/
+cp ~/.dotfiles/vim/colors_vim/vim-colors-solarized/colors/solarized.vim ~/.dotfiles/vim/colors/
 
 # Copying pathogen intovim directory
 cp ~/.dotfiles/vim/pathogen/autoload/pathogen.vim ~/.dotfiles/vim/autoload/ 
