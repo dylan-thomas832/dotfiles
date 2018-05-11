bdd0483fcef9f2e4bd6b9b831d4a3209d2a7e79e
Dylan Thomas <dylan93@vt.edu>
2018-05-11 02:20:45 -0400

    Updated requirements files for Python. Updated rc files to add home/,local/bin for pip install scripts.

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -1,6 +1,8 @@
 # If you come from bash you might have to change your $PATH.
 export PATH=$HOME/.dotfiles/bin:/usr/local/bin:$PATH
 
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+
 # /usr/bin:/usr/sbin:
 
 # Path to your oh-my-zsh installation.
