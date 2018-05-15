af3d35e3bf82282d84c0f6bbe3fce8e7bfdbefba
Dylan Thomas <dylan93@vt.edu>
2018-05-15 13:48:52 -0400

    Added initial support for VS Code files.
    Added a generic script to install extensions from terminal.
    Added a generic settings config file.

---
+++ vscode/install_vscode_extensions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin
+
+set -e
+
+# Packages to install
+code --install-extension teabyii.ayu
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension zhuangtongfa.material-theme
+code --install-extension ms-python.python
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension James-Yu.latex-workshop
+code --install-extension alefragnani.project-manager
+code --install-extension alefragnani.bookmarks
+#code --install-extension
+
