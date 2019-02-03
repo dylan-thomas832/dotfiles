6d091c803b4befb8c4e7635c6846a39dee1ac476
Dylan Thomas <dylan93@vt.edu>
2019-02-03 02:22:36 -0500

    Update vscode dotfiles.

--- vscode/install_vscode_extensions.sh
+++ vscode/install_vscode_extensions.sh
@@ -1,14 +1,26 @@
-#!/usr/bin
+#!/usr/bin/env bash
 
 set -e
 
 # Packages to install
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension yzhang.markdown-all-in-one
-code --install-extension zhuangtongfa.material-theme
-code --install-extension ms-python.python
-code --install-extension robertohuertasm.vscode-icons
-code --install-extension James-Yu.latex-workshop
-code --install-extension alefragnani.project-manager
-code --install-extension alefragnani.bookmarks
 
+# Language support packages
+code --install-extension ms-python.python                           # Python
+code --install-extension ms-vscode.cpptools                         # C++
+code --install-extension James-Yu.latex-workshop                    # Latex
+code --install-extension yzhang.markdown-all-in-one                 # Markdown
+code --install-extension redhat.vscode-yaml                         # YAML
+
+# Language improvement packages
+code --install-extension njpwerner.autodocstring                    # Auto-docstring for Python
+code --install-extension christian-kohler.path-intellisense         # Intellisense for paths
+
+# Themes & icons
+code --install-extension zhuangtongfa.material-theme                # One Dark Pro theme
+code --install-extension robertohuertasm.vscode-icons               # Better icons
+
+# Utility extensions
+code --install-extension alefragnani.project-manager                # Save project workspaces
+code --install-extension alefragnani.bookmarks                      # Bookmark code-navigation
+code --install-extension eamodio.gitlens                            # Improved Git integration
+code --install-extension streetsidesoftware.code-spell-checker      # Spell-checker
