2818aae30ed564b97842825d0865c294c3754170
Dylan Thomas <dylan93@vt.edu>
2019-02-08 18:53:12 -0500

    Added files for linux pre-install.

--- bin/check_wsl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null ; then
-  echo "Need to download font file from: https://github.com/powerline/fonts, open, and install it."
-else
-  echo "Installing fonts."
-  ~/.dotfiles/terminal/fonts/install.sh
-fi
