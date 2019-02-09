aa34b6391bcf91ab07d0fc2526676f3a4f50f151
Dylan Thomas <dylan93@vt.edu>
2019-02-08 19:55:47 -0500

    Updating configs for wsl & linux installs.

--- shells/zsh/zshrc.d/environment.zsh
+++ shells/zsh/zshrc.d/environment.zsh
@@ -3,10 +3,10 @@
 # export PATH=$HOME/anaconda3/bin:$PATH
 
 # pip should only run if there is a virtualenv currently activated
-export PIP_REQUIRE_VIRTUALENV=true
+# export PIP_REQUIRE_VIRTUALENV=true
 
 # Cache pip-installed packages to avoid re-downloading
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
 
 # Python startup file
 export PYTHONSTARTUP="$HOME/.pythonrc"
@@ -18,8 +18,8 @@ export DISPLAY=:0
 export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib
 
 # Local binaries. First should already be added
-# export PATH=/usr/local/bin:$PATH
-# export PATH="$HOME/.local/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 # Path to manpages
 export MANPATH="/usr/local/man:$MANPATH"
@@ -29,6 +29,29 @@ export MANPATH="/usr/local/man:$MANPATH"
 
 # ssh keys
 export SSH_KEY_PATH="~/.ssh/rsa_id"
+
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/"
+[ -n "$PS1" ] && \
+    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+        eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+# Anaconda stuff
+# >>> conda init >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$(CONDA_REPORT_ERRORS=false '~/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    \eval "$__conda_setup"
+else
+    if [ -f "~/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "~/anaconda3/etc/profile.d/conda.sh"
+        CONDA_CHANGEPS1=false conda activate base
+    else
+        \export PATH="~/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda init <<<
 
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
