72157a88d67565b9b076fee77d3e4cdea9820254
Dylan Thomas <dylan93@vt.edu>
2019-02-15 01:57:58 -0500

    Forgot plugins.

---
+++ shells/zsh/zshrc.d/plugins_after.zsh
@@ -0,0 +1,25 @@
+# Check if zplug is installed
+if [[ ! -d ~/.zplug ]]; then
+  git clone https://github.com/zplug/zplug ~/.zplug
+  source ~/.zplug/init.zsh && zplug update --self
+fi
+
+# Essential
+source ~/.zplug/init.zsh
+
+# Make sure to use double quotes to prevent shell expansion
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-history-substring-search"
+
+# Install packages that have not been installed yet
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    else
+        echo
+    fi
+fi
+
+# load plugins
+zplug load
