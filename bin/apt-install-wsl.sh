95cae97790ff10bba3b11a93b8354bde90f20867
Dylan Thomas <dylan93@vt.edu>
2019-02-15 23:19:41 -0500

    Remove oh-my-zsh from auto-linux-setup.

--- bin/apt-install-wsl.sh
+++ bin/apt-install-wsl.sh
@@ -27,9 +27,6 @@ apt install -y ack-grep silversearcher-ag
 # Remove unneeded packages
 apt autoremove -y
 
-# Setup oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
 # Change to zsh
 chsh -s $(/bin/zsh)
 
