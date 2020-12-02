7cbde32ad1b8c20dfbd23637c8723a2c329765b0
Dylan Thomas <dylan.thomas@vt.edu>
2020-12-02 02:20:58 -0500

    Add keybinds for home, end, and arrow navigation.

--- shells/zsh/zshrc.d/plugins_after.zsh
+++ shells/zsh/zshrc.d/plugins_after.zsh
@@ -2,6 +2,13 @@
 bindkey "^R" history-incremental-search-backward
 bindkey "^[[A" history-beginning-search-backward
 bindkey "^[[B" history-beginning-search-forward
+# Home, End, Delete keys
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[3~" delete-char
+# CTRL + Left/Right
+bindkey "^[[1;5D" backward-word
+bindkey "^[[1;5C" forward-word
 
 # Syntax highlighting from zsh-users
 source ~/.zshrc.d/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
