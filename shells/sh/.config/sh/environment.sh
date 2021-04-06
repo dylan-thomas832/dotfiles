60a58ded4e8b4cccc827fc06fbcaea4bcdb72486
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-06 19:02:51 -0400

    Fix incorrect path. Set XDG dirs.

--- shells/sh/.config/sh/environment.sh
+++ shells/sh/.config/sh/environment.sh
@@ -5,6 +5,19 @@ export VISUAL=vim
 # Cache pip-installed packages to avoid re-downloading
 export PIP_DOWNLOAD_CACHE="$HOME/.cache/pip"
 
+# XDG Base Directory
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+
+# Change history directories for bash and less
+export LESSHISTFILE="${XDG_CACHE_HOME}/less/history"
+export HISTFILE="${XDG_CACHE_HOME}/bash/history"
+
+# Change history directories for redis and sqlite
+export SQLITE_HISTORY="${XDG_CACHE_HOME}/sqlite/history"
+export REDISCLI_HISTFILE="${XDG_CACHE_HOME}/redis/history"
+
 # Python startup file
 export PYTHONSTARTUP="$HOME/.config/python/pythonrc.py"
 
@@ -16,6 +29,9 @@ export CONDARC="$HOME/.config/conda/condarc"
 
 # Set IPython directory
 export IPYTHONDIR="$HOME/.config/ipython"
+
+# Set pylint home
+export PYLINTHOME="${XDG_DATA_HOME}/pylint"
 
 # Path to manpages
 export MANPATH="/usr/local/man:$MANPATH"
