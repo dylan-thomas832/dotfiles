2e72cf58215e0372c7a6098f39f8807232c826a2
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-06 17:24:49 -0400

    Fix windows bash config files to use common configs.

--- terminal/cmder/user_profile.sh
+++ terminal/cmder/user_profile.sh
@@ -7,148 +7,26 @@
 # . "${CMDER_ROOT}/vendor/lib/start-ssh-agent.sh"
 
 # Adds git completions
-source ~/.git-completion.bash
+source ~/.config/git-completion.bash
+
+# Source standard linux bashrc for common configurations
+source ~/.bashrc
 
 ####### Aliases
-
-# Easier navigation: .., ..., ...., ....., ~ and -
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
 
 # open epxlorer here
 alias e=explorer
 
-# Ensure VIM & lazy git
-alias vi=vim
-alias g=git
+# Lazy git
 __git_complete g _git
 
 # CD to cmder root
 alias cmderr="cd ${CMDER_ROOT}"
 
-# List files colorized in long format
-alias l="ls -lF --color --show-control-chars"
-alias ls="ls -lF --color --show-control-chars"
-# List all files colorized in long formation
-alias la="ls -lAF --color --show-control-chars"
-# List only directories
-alias lsd="ls -lF --color | grep --color=never '^d'"
-
-# Colorize `grep` output
-alias grep="grep --color=auto"
-
-# Get the week number
-alias week="date +%V"
-
-# Reload the shell (i.e. invoke as a login shell)
-alias reload="exec ${SHELL} -l"
-
-# Print public key to screen
-alias pubkey="cat ~/.ssh/id_rsa.pub"
-
-# Print each PATH entry on a separate line
-alias path='echo -e ${PATH//:/\\n}'
-
-# Open git repo in browser
-alias gh="explorer \`git remote -v | grep fetch | head -1 | cut -f2 | cut -d' ' -f1 | sed -e's/git@/http:\/\//' -e's/\.git$//' | sed -E 's/(\/\/[^:]*):/\1\//'\`"
-
 ###### Exports
-
-# Local binaries. First should already be added
-export PATH="/usr/local/bin:$PATH"
-export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/bin:$PATH"
 
 # Set terminal appropriately
 export TERM='cygwin'
 
-# Save python config & allow symlinks
-export PYTHONSTARTUP="$HOME/.pythonrc"
+# Allow symlinks
 export MSYS=winsymlinks:nativestrict
-
-# Cache pip-installed packages to avoid re-downloading
-export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
-
-# You may need to manually set your language environment
-export LANG=en_US.UTF-8
-export LC_ALL='en_US.UTF-8';
-
-# Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
-export PYTHONIOENCODING='UTF-8';
-
-# ssh keys
-export SSH_KEY_PATH="~/.ssh/rsa_id"
-
-# Make vim default editor
-export EDITOR='vim'
-
-###### Functions
-
-# Timestamp function
-timestamp () {
-    python -c 'TIMESTAMP_FORMAT="%d-%m-%Y_%H-%M-%S";import time;print(time.strftime(TIMESTAMP_FORMAT, time.gmtime()))'
-}
-
-# Create a new directory and enter it
-function mkd() {
-    mkdir -p "$@" && cd "$_";
-}
-
-# Create a .tar.gz archive, using `zopfli`, `pigz` or `gzip` for compression
-function targz() {
-	local tmpFile="${@%/}.tar";
-	tar -cvf "${tmpFile}" --exclude=".DS_Store" "${@}" || return 1;
-
-	size=$(
-		stat -f"%z" "${tmpFile}" 2> /dev/null; # macOS `stat`
-		stat -c"%s" "${tmpFile}" 2> /dev/null;  # GNU `stat`
-	);
-
-	local cmd="";
-	if (( size < 52428800 )) && hash zopfli 2> /dev/null; then
-		# the .tar file is smaller than 50 MB and Zopfli is available; use it
-		cmd="zopfli";
-	else
-		if hash pigz 2> /dev/null; then
-			cmd="pigz";
-		else
-			cmd="gzip";
-		fi;
-	fi;
-
-	echo "Compressing .tar ($((size / 1000)) kB) using \`${cmd}\`…";
-	"${cmd}" -v "${tmpFile}" || return 1;
-	[ -f "${tmpFile}" ] && rm "${tmpFile}";
-
-	zippedSize=$(
-		stat -f"%z" "${tmpFile}.gz" 2> /dev/null; # macOS `stat`
-		stat -c"%s" "${tmpFile}.gz" 2> /dev/null; # GNU `stat`
-	);
-
-	echo "${tmpFile}.gz ($((zippedSize / 1000)) kB) created successfully.";
-}
-
-# Compare original and gzipped file size
-function gz() {
-	local origsize=$(wc -c < "$1");
-	local gzipsize=$(gzip -c "$1" | wc -c);
-	local ratio=$(echo "$gzipsize * 100 / $origsize" | bc -l);
-	printf "orig: %d bytes\n" "$origsize";
-	printf "gzip: %d bytes (%2.2f%%)\n" "$gzipsize" "$ratio";
-}
-
-# Determine size of a file or total size of a directory
-function fs() {
-	if du -b /dev/null > /dev/null 2>&1; then
-		local arg=-sbh;
-	else
-		local arg=-sh;
-	fi
-	if [[ -n "$@" ]]; then
-		du $arg -- "$@";
-	else
-		du $arg .[^.]* ./*;
-	fi;
-}
