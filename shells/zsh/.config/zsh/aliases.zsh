3f04aa3aff2e86181b281f53ed45e7b711d21b61
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-06 21:34:59 -0400

    Remove aliases that break colorized dirs

--- shells/zsh/.config/zsh/aliases.zsh
+++ shells/zsh/.config/zsh/aliases.zsh
@@ -2,7 +2,3 @@
 
 # Open git repo in browser
 alias gh="xdg-open \`git remote -v | grep fetch | head -1 | cut -f2 | cut -d' ' -f1 | sed -e's/git@/http:\/\//' -e's/\.git$//' | sed -E 's/(\/\/[^:]*):/\1\//'\`"
-
-# Kick values from being included in the ZSH history
-alias ls=' ls'
-alias cd=' cd'
