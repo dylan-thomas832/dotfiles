4d62b8813586c7b3a3847c8892c7207924288e01
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-06 21:24:56 -0400

    Move base16 and dircolor into generic sh configs.

--- shells/sh/.config/sh/common.sh
+++ shells/sh/.config/sh/common.sh
@@ -80,3 +80,12 @@ if [ -x /usr/bin/fzf ] ; then
     [ -f /usr/share/fzf/key-bindings.${_sh} ] && source /usr/share/fzf/key-bindings.${_sh}
     [ -f /usr/share/fzf/completion.${_sh} ] && source /usr/share/fzf/completion.${_sh}
 fi
+
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/"
+[ -n "$PS1" ] && \
+    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+        eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+# Colors
+eval $(dircolors -b ${HOME}/.dircolors)
