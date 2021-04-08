9bca9b5d4f9ef9d436fcb081b7bd6dac02c2c886
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-07 23:50:17 -0400

    Add sudo alias.

--- shells/zsh/aliases.zsh
+++ shells/zsh/aliases.zsh
@@ -3,6 +3,9 @@
 # Open git repo in browser
 alias gh="xdg-open \`git remote -v | grep fetch | head -1 | cut -f2 | cut -d' ' -f1 | sed -e's/git@/http:\/\//' -e's/\.git$//' | sed -E 's/(\/\/[^:]*):/\1\//'\`"
 
+# If the last character of the alias value is a space or tab character, then the next command word following the alias is also checked for alias expansion.
+alias sudo='sudo '
+
 # Global aliases -> directly replaced on line
 alias -g ...='../..'
 alias -g ....='../../..'
