0b1c5fc3481a02d4987bb613b45f632a935ad301
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-09 12:42:42 -0400

    Change auto-suggest color.

--- shells/zsh/plugins_after.zsh
+++ shells/zsh/plugins_after.zsh
@@ -10,10 +10,14 @@
 
 # Syntax highlighting from zsh-users
 source ~/.config/zsh/vendor/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 # Substring search from zsh-users
 source ~/.config/zsh/vendor/zsh-history-substring-search/zsh-history-substring-search.zsh
+
 # Auto suggestions (pre-typed)
 source ~/.config/zsh/vendor/zsh-autosuggestions/zsh-autosuggestions.zsh
+# Customize the auto-suggest foreground color
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=075'
 
 # Custom ZSH plugins
 
