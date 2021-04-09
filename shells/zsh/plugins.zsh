09fb700c849f6f53010a2089ac6a6ee9917e6681
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-09 05:41:56 -0400

    Make fzf and ag work together.

--- shells/zsh/plugins.zsh
+++ shells/zsh/plugins.zsh
@@ -31,5 +31,11 @@ function _pip_completion {
 }
 compctl -K _pip_completion python -m pip
 
-# FZF
+# FZF sourcing
 [ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ] && source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
+# Force FZF to use ag. -g "" allow ag to search filenames and pipe to fzf
+export FZF_DEFAULT_COMMAND='ag --hidden --path-to-ignore ~/.config/agignore -g ""'
+# # Copy command to fzf keyboard shortcuts
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS="--reverse --preview 'cat {}' --color=bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672"
