8fb1b2b2d83b13d798e9aee555f55afd876c902f
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-09 05:35:07 -0400

    Make alias for default ag command.

--- shells/zsh/aliases.zsh
+++ shells/zsh/aliases.zsh
@@ -6,6 +6,9 @@ alias gh="xdg-open \`git remote -v | grep fetch | head -1 | cut -f2 | cut -d' ' 
 # If the last character of the alias value is a space or tab character, then the next command word following the alias is also checked for alias expansion.
 alias sudo='sudo '
 
+# Force ag to use ignore file and not ignore hidden files by default
+alias ag='ag --hidden --path-to-ignore ~/.config/agignore'
+
 # Global aliases -> directly replaced on line
 alias -g ...='../..'
 alias -g ....='../../..'
