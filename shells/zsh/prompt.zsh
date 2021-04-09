29e73a4a198e9faffa12a50f259413e47527a013
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-09 04:17:37 -0400

    Add exit code, hostname, and limit path for prompt.

--- shells/zsh/prompt.zsh
+++ shells/zsh/prompt.zsh
@@ -23,9 +23,23 @@ precmd() {
 _setup_ps1() {
   vcs_info
   GLYPH="▲"
+  # Grabs last bit of path in $CONDA_DEFAULT_ENV. This is updated when `conda activate` is called
   CONDA_CURR_ENV=$(basename "$CONDA_DEFAULT_ENV")
+  # Flips the glyph if in vi command mode
   [ "x$KEYMAP" = "xvicmd" ] && GLYPH="▼"
-  PS1="$CONDA_CURR_ENV %(?.%F{blue}.%F{red})$GLYPH%f %(1j.%F{cyan}[%j]%f .)%F{blue}%~%f %(!.%F{red}#%f .)"
+  # Sets the glyph for vi-mode (up/down) and previous command exit code (color)
+  local glyph="%(?.%F{blue}.%F{red}%? )$GLYPH%f"
+  # Sets the symbol used for user prompt
+  local prompt_symbol="%(!.%F{red}#%f .)"
+  # Includes the hostname if logged in using SSH
+  local ssh_hostname="%m "
+  [[ -z "${SSH_CONNECTION}" ]] && ssh_hostname=""
+  # Includes the number of background jobs currently running if > 0
+  local background_jobs="%(1j.%F{cyan}[%j]%f .)"
+  # Includes the cwd and 3 trailing components.
+  # If the current working directory starts with $HOME, that part is replaced by a ‘~’
+  local cwd=%F{blue}%3~%f
+  PROMPT="$CONDA_CURR_ENV $glyph $ssh_hostname$background_jobs$cwd $prompt_symbol"
   RPROMPT="$vcs_info_msg_0_"
 }
 _setup_ps1
