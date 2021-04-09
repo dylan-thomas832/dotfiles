6eb7e8ae66e225fdf32121080e6d02adc26ca43b
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-09 11:48:30 -0400

    Update fzf settings, move pfetch into plugins.

--- shells/zsh/plugins.zsh
+++ shells/zsh/plugins.zsh
@@ -32,10 +32,18 @@ function _pip_completion {
 compctl -K _pip_completion python -m pip
 
 # FZF sourcing
-[ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ] && source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
+[[ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ]] && source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
+
 # Force FZF to use ag. -g "" allow ag to search filenames and pipe to fzf
-export FZF_DEFAULT_COMMAND='ag --hidden --path-to-ignore ~/.config/agignore -g ""'
-# # Copy command to fzf keyboard shortcuts
+# export FZF_DEFAULT_COMMAND='ag --hidden --path-to-ignore ~/.config/agignore -g ""'
+
+# Force FZF to use fd instead of find
+# export FZF_DEFAULT_COMMAND="fdfind --type f --hidden --ignore-file ~/.config/agignore"
+
+# # Copy command to fzf keyboard shortcuts. Set default fzf options.
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS="--reverse --preview 'cat {}' --color=bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672"
+
+# For running pfetch if interested
+export PF_INFO="ascii title os host kernel de shell uptime pkgs memory palette"
