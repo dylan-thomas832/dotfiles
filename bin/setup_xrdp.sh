baeb54c3233c9b3801d2ef933bc8e9cabbd7e517
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-13 01:02:51 -0400

    Fix xrdp setup.

--- bin/setup_xrdp.sh
+++ bin/setup_xrdp.sh
@@ -2,8 +2,15 @@
 
 ## This script installs all requirements/dependencies for xrdp on Ubuntu WSL
 
+# Install xrdp package
 sudo apt install xrdp
+
+# Back up default config files
 sudo cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
 sudo cp /etc/xrdp/sesman.ini /etc/xrdp/sesman.ini.bak
+
+# Direct error output because it blows up quickly
 sudo ln -s /dev/null ~/.xsession-errors
-sudo cp 
+
+# Start xrdp service
+sudo service xrdp start
