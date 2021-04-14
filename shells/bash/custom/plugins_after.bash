d0be159d6ea199aa32c9e33a9754381798a1975b
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-13 21:17:45 -0400

    Add conda setup to std bashrc & zshrc plugins.

--- shells/bash/custom/plugins_after.bash
+++ shells/bash/custom/plugins_after.bash
@@ -28,3 +28,20 @@ export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap 
 # ALT+C uses fd with toggle-able directory tree preview
 export FZF_ALT_C_COMMAND="fdfind -t d . "
 export FZF_ALT_C_OPTS="--reverse --preview 'tree -adC {} | head -200' --bind '?:toggle-preview'"
+
+# Conda setup
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
--- shells/zsh/custom/plugins_after.zsh
+++ shells/zsh/custom/plugins_after.zsh
@@ -28,7 +28,7 @@ export PF_INFO="ascii title os host kernel de shell uptime pkgs memory palette"
 [[ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ]] && source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
 
 # Force FZF to use rg
-export FZF_DEFAULT_COMMAND='rg --files --hidden --color=auto --follow --ignore-file ~/.config/agignore'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --color=auto --smart-case --follow --ignore-file ~/.config/agignore'
 
 # Force FZF to use ag. -g "" allow ag to search filenames and pipe to fzf
 # export FZF_DEFAULT_COMMAND='ag --hidden --path-to-ignore ~/.config/agignore -g ""'
@@ -49,3 +49,20 @@ export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap 
 # ALT+C uses fd with toggle-able directory tree preview
 export FZF_ALT_C_COMMAND="fdfind -t d . "
 export FZF_ALT_C_OPTS="--reverse --preview 'tree -adC {} | head -200' --bind '?:toggle-preview'"
+
+# Conda setup
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
