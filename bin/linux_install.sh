4616ebc162396a8e66907892ae534068490154f6
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-14 01:37:18 -0400

    Add alacritty, bat, delta into main linux install.

--- bin/linux_install.sh
+++ bin/linux_install.sh
@@ -15,16 +15,33 @@ apt install -y python3-dev python3-pip python3-tk
 apt install -y libopenblas-dev libatlas-base-dev liblapack-dev
 
 # Esnure we have basics
-apt install -y git wget curl vim ctags
+apt install -y git wget curl vim ctags xdg-utils
 
 # Get zsh
 apt install -y zsh
 
-# Get Ack & Ag
-apt install -y ack-grep silversearcher-ag fd-find tree
-# Work around for bat and ripgrep package discrepancies
-# From: https://github.com/sharkdp/bat/issues/938#issuecomment-646745610
-sudo apt install -o Dpkg::Options::="--force-overwrite" bat ripgrep
+# Get Ack, Ag, fd, tree, and ripgrep for niceties!
+apt install -y ack-grep silversearcher-ag fd-find tree ripgrep
+
+# Get Rust installer and run it
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path --verbose
+rustup override set stable
+rustup update stable
+
+# Apt dependencies for Alacritty
+sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+
+## Cargo installs
+
+# Install Alacritty (fast terminal emulator)
+cargo install alacritty
+
+# Install bat (cat, but with color)
+cargo install --locked bat
+
+# Install delta (better termina git diff viewer)
+cargo install git-delta
+
 
 # Remove unneeded packages
 apt autoremove -y
