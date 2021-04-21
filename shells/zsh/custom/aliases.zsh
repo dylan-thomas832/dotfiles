a6a9af443a7dc164a09013a88ae918ea552a7382
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-20 21:29:50 -0400

    Add zcalc aliases and rc file.

--- shells/zsh/custom/aliases.zsh
+++ shells/zsh/custom/aliases.zsh
@@ -17,6 +17,10 @@ alias gh="xdg-open \`git remote -v | grep fetch | head -1 | cut -f2 | cut -d' ' 
 # If the last character of the alias value is a space or tab character, then the next command word following the alias is also checked for alias expansion.
 alias sudo='sudo '
 
+# Zcalc in floating point mode
+alias calc='zcalc -f'
+alias zcalc='zcalc -f'
+
 # Force ag to use ignore file and not ignore hidden files by default
 alias ag="ag --hidden --ignore '.git'"
 alias rg="rg --hidden --glob '!.git'"
