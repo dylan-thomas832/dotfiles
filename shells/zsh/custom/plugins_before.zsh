50a104c2f54228c0bf3368b92250bb1446f3ed3f
Dylan Thomas <dylan.thomas@vt.edu>
2021-04-21 02:37:36 -0400

    Add initial zfunc, and support for autoloading.

--- shells/zsh/custom/plugins_before.zsh
+++ shells/zsh/custom/plugins_before.zsh
@@ -13,7 +13,11 @@
 #####################
 
 # Add custom ZSH functions
-[[ -n $ZFUNCSDIR ]] && fpath+=$ZFUNCSDIR
+[[ -n $ZFUNCSDIR ]] && {
+    fpath+=$ZFUNCSDIR
+    fns=($ZFUNCSDIR/**/*(.))
+    (( $#fns )) && autoload -Uz "$fns[@]"
+}
 
 ##########################
 ### Custom ZSH Plugins ###
@@ -21,11 +25,11 @@
 
 # [NOTE]: Retrieved via: `pip completion --zsh`
 function _pip_completion {
-  local words cword
-  read -Ac words
-  read -cn cword
-  reply=( $( COMP_WORDS="$words[*]" \
-             COMP_CWORD=$(( cword-1 )) \
-             PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null ))
+    local words cword
+    read -Ac words
+    read -cn cword
+    reply=( $( COMP_WORDS="$words[*]" \
+               COMP_CWORD=$(( cword-1 )) \
+               PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null ))
 }
 compctl -K _pip_completion python -m pip
